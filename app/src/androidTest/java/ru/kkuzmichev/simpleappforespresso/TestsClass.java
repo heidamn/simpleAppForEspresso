fa5bc1dc4b63c7aab9c93cda75b741fee8eb2e43
Даниил <sholomov.d@panoramik.ru>
2022-04-02 18:04:39 +0300

    lab 3

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/TestsClass.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/TestsClass.java
@@ -12,31 +12,53 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
 import static org.hamcrest.Matchers.allOf;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Environment;
 import android.view.Gravity;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.uiautomator.UiDevice;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
-@RunWith(AndroidJUnit4.class)
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+import io.qameta.allure.kotlin.Allure;
+
+@RunWith(AllureAndroidJUnit4.class)
 public class TestsClass {
 
     @Before //Выполняется перед тестами
     public void registerIdlingResources() { //Подключаемся к “счетчику”
         IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource);
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        //this is the key part
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //this is the key part
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
     }
 
     @After // Выполняется после тестов
@@ -46,6 +68,45 @@ public class TestsClass {
 
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule
+            .grant(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            );
+
+    @Rule
+    public TestWatcher watcher = new TestWatcher() {
+
+        @Override
+        protected void failed(Throwable e, Description description) {
+            String className = description.getClassName();
+            className = className.substring(className.lastIndexOf('.') + 1);
+            String methodName = description.getMethodName();
+            takeScreenshot(className + "#" + methodName);
+        }
+
+        private void takeScreenshot(String name) {
+            File path = new File(Environment.getExternalStorageDirectory()
+                    .getAbsolutePath() + "/screenshots/");
+
+            if (!path.exists()) {
+                path.mkdirs();
+            }
+
+            UiDevice device = UiDevice.getInstance(getInstrumentation());
+            String filename = name + ".png";
+
+            device.takeScreenshot(new File(path, filename));
+
+            try {
+                Allure.attachment(filename, new FileInputStream(new File(path, filename)));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    };
 
     @Test
     public void positiveTest() {
@@ -86,11 +147,11 @@ public class TestsClass {
     }
 
     @Test
-    public void intentTest() {
-        Intents.init();
+    public void intentTest() throws InterruptedException {
         ViewInteraction options = onView(withContentDescription("More options"));
         options.perform(click());
         ViewInteraction settings = onView(withText("Settings"));
+        Intents.init();
         settings.perform(click());
         intended(hasData("https://google.com"));
         intended(hasAction(Intent.ACTION_VIEW));
